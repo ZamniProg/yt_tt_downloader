17e9a12e229b5e7327f209018b54a420246e55d5
ZamniProg <artemopbgsg@gmail.com>
2025-01-07 01:46:56 +0300

    little update

--- main.py
+++ main.py
@@ -2,25 +2,38 @@ import yt_dlp
 
 
 TELEGRAM_TOKEN = ""
+QUAL = {'720': '22', '360': '18', '480': '135', '1080': '137', 'audio': '140'}
 
 
-def download_video_from_yt(url: str, quality: str = 'best', file_format: str = 'mp4'):
+def download_video_from_yt(url: str, quality: str = 'best'):
     ydl_opts = {
         'format': quality,
         'outtmpl': f'downloads/%(title)s.%(ext)s',
         'noplaylist': True,
-        'postprocessors': [{
-            'key': 'FFmpegVideoConvertor',
-            'preferedformat': file_format,
-        }],
     }
+    if quality != '140':
+        ydl_opts['postprocessors'] = [{
+            'key': 'FFmpegVideoConvertor',
+            'preferedformat': 'mp4',
+        }]
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info_dict = ydl.extract_info(url, download=True)
-        return ydl.prepare_filename(info_dict)
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(url, download=True)
+            formats = info_dict.get('formats', [])
+
+            if quality not in formats:
+                print("Error: The format you selected is unavailable for this video")
+            else:
+                return ydl.prepare_filename(info_dict)
+    except yt_dlp.utils.DownloadError as e:
+        print(f"Error: {e}")
+        return
+    except Exception as e:
+        print(f"Unexpected error: {e}")
 
 
 url = input('Paste link: ')
-quality = str(input('Paste Quality: '))
+quality = QUAL[input('Paste Quality: ')]
 
 download_video_from_yt(url, quality)
